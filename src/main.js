a865cc0dbf777c6680787f51d6d2f776b1885a4b
Daniela Paz <danielalizama.a@gmail.com>
2022-07-26 15:43:44 -0400

    css video

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// import { example } from './data.js';
+// // import data from './data/lol/lol.js';
+// import data from './data/harrypotter/';
+// // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+// console.log(example, data);
+let allCharacter;
+
+fetch("https://raw.githubusercontent.com/Seabird15/SCL021-data-lovers/main/src/data/harrypotter/harry.json")
+.then(function(response) {
+return response.json();
+}) //traer data
+.then(function(data) {
+allCharacter= data.harry;
+console.log(allCharacter)
+})
